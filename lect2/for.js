477c78136a42fbbb98220830e1448bffb80082a7
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-07 10:17:19 +0530

    loops and functions

---
+++ lect2/for.js
@@ -0,0 +1,21 @@
+//for loop
+
+let arr=[1,2,3,4,5,true,"string"];
+for(let a in arr){
+    console.log(a)
+}
+
+//for of loop
+let arr=[1,2,3,4,5,true,"string"];
+for(let a of arr){
+    console.log(a)
+}
+
+//object
+let student ={
+    id:1,
+    name:"om"
+}
+console.log(student.id);
+
+//functions
