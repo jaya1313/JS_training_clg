5c9298b06ce386a4ff5d6bbc37c21fc3a2e94ba3
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-09 23:18:53 +0530

    js_basics

--- lect1/index.js
+++ lect1/index.js
@@ -1,32 +1,37 @@
-console.log("Hello")
-        let a=5;
-        console.log(a);
-        console.error("error");
-        console.warn("hello");
-        //alert("kaun ho tum?")
-        console.log(5+5);
-        console.log(5+'5');
-        console.log(5-'5');
-        console.log(5==5);
-        console.log(5=='5');
-        console.log(5==='5');
-        console.log(false+5);
-        console.log(null + null); //numeric value of null is 0
-        console.log(null - null); 
-        console.log(undefined-1); //NaN (not a number)
-        console.log(null+"hii");
+// console.log("Hello")
+//         let a=5;
+//         console.log(a);
+//         console.error("error");
+//         console.warn("hello");
+//         //alert("kaun ho tum?")
+//         console.log(5+5);
+//         console.log(5+'5');
+//         console.log(5-'5');
+//         console.log(5==5);
+//         console.log(5=='5');
+//         console.log(5==='5');
+//         console.log(false+5);
+//         console.log(null + null); //numeric value of null is 0
+//         console.log(null - null); 
+//         console.log(undefined-1); //NaN (not a number)
+//         console.log(null+"hii");
 
-        let arr=[]
-        let arr1=[]
-        console.log(arr == arr1)
-        console.log(arr === arr1)
+//         let arr=[]
+//         let arr1=[]
+//         console.log(arr == arr1)
+//         console.log(arr === arr1)
 
-        if(null){ //falsi values = false, 0, undefined
-            console.log("hello")
-        }else{
-            console.log("hii")
-        }
-        let score=100
-        let to=score==100
-        console.log(to)
+//         if(null){ //falsi values = false, 0, undefined
+//             console.log("hello")
+//         }else{
+//             console.log("hii")
+//         }
+//         let score=100
+//         let to=score==100
+//         console.log(to)
         
+// for loop
+let arr=[]
+for(let a in arr){
+    console.log(a)
+}
