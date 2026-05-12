0ca2c9f4af2b65c0fa5ed35e148c3ee5ecc8907d
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-12 12:33:17 +0530

    DOM & calculator

--- lect5/index.js
+++ lect5/index.js
@@ -1,23 +1,30 @@
-let h1 = document.querySelector("h1");
-let h2 = document.querySelector("h2");
+// let h1 = document.querySelector("h1");
+// let h2 = document.querySelector("h2");
 
-//user input
-// name = prompt("enter your name");
-// h2.innerText = "name"+ ":"+ name;
+// //user input
+// // name = prompt("enter your name");
+// // h2.innerText = "name"+ ":"+ name;
 
-//name.innerHTML += prompt("Enter your name: ");
+// //name.innerHTML += prompt("Enter your name: ");
 
-//create element
-// let p = document.createElement("p");
-// p.innerText = "this is new paragraph";
-// h2.appendChild(p); //append the new paragraph to h2
+// //create element
+// // let p = document.createElement("p");
+// // p.innerText = "this is new paragraph";
+// // h2.appendChild(p); //append the new paragraph to h2
 
-// input event listner
-// let inp = document.querySelector("input")
+// // input event listner
+//  let inp = document.querySelector("input")
 // inp.addEventListener("input",(e)=>{
 //        console.log("hello")
 //        console.log(e.target.value);
 //        h2.innerHTML = e.target.value;
 // })
 
-//
+// let allData=[]
+// let btn = document.querySelector("button");
+// let h3 = document.querySelector("h3");
+// btn.addEventListener("click",()=>{
+//     let data = inp.value;
+//     allData.push(data);
+//     h3.innerHTML = allData
+// })
