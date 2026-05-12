8686f7d28d061ae7063c307878ecf0c930fb2d10
Jaya Karwani <jayakarwani4@gmail.com>
2026-05-12 11:15:40 +0530

    DOM

---
+++ lect5/index.js
@@ -0,0 +1,23 @@
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+//user input
+// name = prompt("enter your name");
+// h2.innerText = "name"+ ":"+ name;
+
+//name.innerHTML += prompt("Enter your name: ");
+
+//create element
+// let p = document.createElement("p");
+// p.innerText = "this is new paragraph";
+// h2.appendChild(p); //append the new paragraph to h2
+
+// input event listner
+// let inp = document.querySelector("input")
+// inp.addEventListener("input",(e)=>{
+//        console.log("hello")
+//        console.log(e.target.value);
+//        h2.innerHTML = e.target.value;
+// })
+
+//
